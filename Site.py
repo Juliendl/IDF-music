49f60a6ab3f3a1e76873240b55f3f2a07370a7ec
Juliendl <66421805+Juliendl@users.noreply.github.com>
2021-08-28 13:37:49 +0200

    V1 automated search
    working with final.csv as database

--- Site.py
+++ Site.py
@@ -1,47 +1,56 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import json
-from youtubesearchpython import SearchVideos # YouTube search tool
+from youtubesearchpython import SearchVideos  # YouTube search tool
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials #To access authorised Spotify data
+import json
+from spotipy.oauth2 import SpotifyClientCredentials  # To access authorised Spotify data
+from sklearn.preprocessing import MinMaxScaler
 
 client_id = "11e5271cd71241ba81d4f815ff5dc232"
 client_secret = "01312af2680947dd8e6a55da36bf4056"
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager) #spotify object to access API
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)  # spotify object to access API
 
-sp.trace=False
+sp.trace = False
+
+
 # Get a Song str search
 def getMusicName(elem):
     return '{} - {}'.format(elem['artist'], elem['name'])
 
 
 # Function to search a YouTube Video
+
 def youtubeSearchVideo(music, results=1):
     searchJson = SearchVideos(music, offset=1, mode="json", max_results=results).result()
     searchParsed = json.loads(searchJson)
     searchParsed = searchParsed['search_result'][0]
-    return {'title': searchParsed['title'], \
-            'duration': searchParsed['duration'], \
-            'views': searchParsed['views'], \
-            'url': searchParsed['link'] }
+    return {'title': searchParsed['title'],
+            'duration': searchParsed['duration'],
+            'views': searchParsed['views'],
+            'url': searchParsed['link']}
+
 
 # K-query
 def knnQuery(queryPoint, df, k):
     transf = MinMaxScaler(feature_range=(0, 1), copy=True).fit(df)
     tmp = pd.DataFrame(transf.transform(df))
     scaledQueryPoint = pd.DataFrame(transf.transform(queryPoint))
-    tmp['dist'] = tmp.apply(lambda x: np.linalg.norm(x-scaledQueryPoint.loc[0]), axis=1)
+    tmp['dist'] = tmp.apply(lambda x: np.linalg.norm(x - scaledQueryPoint.loc[0]), axis=1)
     tmp = tmp.sort_values('dist')
     return tmp.head(k).index
 
+
 # Range query
-def rangeQuery(queryPoint, arrCharactPoints, radius):
+def rangeQuery(queryPoint,
+               arrCharactPoints,
+               radius):
     tmp = arrCharactPoints.copy(deep=True)
-    tmp['dist'] = tmp.apply(lambda x: np.linalg.norm(x-queryPoint), axis=1)
+    tmp['dist'] = tmp.apply(lambda x: np.linalg.norm(x - queryPoint), axis=1)
     tmp['radius'] = tmp.apply(lambda x: 1 if x['dist'] <= radius else 0, axis=1)
     return tmp.query('radius == 1').index
+
 
 # Execute k-NN removing the 'query point'
 # def querySimilars(df, columns, idx, func, param):
@@ -56,23 +65,24 @@ def querySimilars2(df, col, queryPoint, func, param):
     response = func(queryPoint[col], df[col], param)
     return response
 
+
 def dfQuery(songToSearch):
     response = sp.search(songToSearch)['tracks']['items'][0]
-    
-    songFeatures = {} #Creates dictionary for that specific album
 
-    #Create keys-values dictionary for song
+    songFeatures = {}  # Creates dictionary for that specific album
+
+    # Create keys-values dictionary for song
     songFeatures['album'] = response['album']['name']
     songFeatures['artist'] = response['album']['artists'][0]['name']
     songFeatures['track_number'] = response['track_number']
     songFeatures['id'] = response['id']
     songFeatures['name'] = response['name']
     songFeatures['uri'] = response['uri']
-    
-    #pull audio features per track
+
+    # pull audio features per track
     features = sp.audio_features(songFeatures['uri'])
 
-    #Append to relevant key-value
+    # Append to relevant key-value
     songFeatures['acousticness'] = features[0]['acousticness']
     songFeatures['danceability'] = features[0]['danceability']
     songFeatures['energy'] = features[0]['energy']
@@ -82,52 +92,57 @@ def dfQuery(songToSearch):
     songFeatures['speechiness'] = features[0]['speechiness']
     songFeatures['tempo'] = features[0]['tempo']
     songFeatures['valence'] = features[0]['valence']
-    #popularity is stored elsewhere
+    # popularity is stored elsewhere
     pop = sp.track(songFeatures['uri'])
     songFeatures['popularity'] = pop['popularity']
-    
+
     df = pd.DataFrame.from_dict([songFeatures])
-    
+
     return df
-   
-def Similarities(songToSearch, k=3):
+
+
+def similarities(songToSearch, k=4):
     """search has to be a string, for example 'Artist - Title' """
-    
+
     # Get audio features of the song from Spotify
     dfResult = dfQuery(songToSearch)
-    
-    #Select parameters for the analysis
-    col = ['acousticness','danceability','energy','instrumentalness','liveness','speechiness','valence']
+
+    # Select parameters for the analysis
+    col = ['acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'speechiness', 'valence']
     dfSongs, func = pd.read_csv('final.csv'), knnQuery
     del dfSongs["Unnamed: 0"]
 
     similarSongs = querySimilars2(dfSongs, col, dfResult, func, k)
-    
-    # Select a song
-    anySong = dfResult.loc[0]
-    # Get the song name
-    anySongName = getMusicName(anySong)
-    # Retrive a YouTube link
-    youtube = youtubeSearchVideo(anySongName)
 
-    # Print
-    print('# Query Point')
-    print(songIndex, anySongName)
-    print(youtube['url'])
-    
-    print('# Similar songs')
+    # This part was useful in the previous version but not in streamlit version. Might be re-introduced in the future.
+    # # Select a song
+    # anySong = dfResult.loc[0]
+    # # Get the song name
+    # anySongName = getMusicName(anySong)
+    # # Retrive a YouTube link
+    # # youtube = youtubeSearchVideo(anySongName)
+
+    # This part was useful in the previous version but not in streamlit version.
+    # # Print
+    # print('# Query Point')
+    # print(anySongName)
+    # # print(youtube['url'])
+
+    # print('# Similar songs')
+    res = []
     for idx in similarSongs:
         anySong = dfSongs.loc[idx]
         anySongName = getMusicName(anySong)
-        youtube = youtubeSearchVideo(anySongName)
+        res.append(anySongName)
+        # youtube = youtubeSearchVideo(anySongName)
 
-        print(idx, anySongName)
-        print(youtube['url'])   
-   
-   
+        # print(anySongName)
+        # print(youtube['url'])
+    return res
+
 
 st.title('''
-   Welcome to the music recommendation system of IDF Music !      
+   Welcome to the music recommendation system of IDF Music !
 ''')
 
 # input = ['Bal musette', 'Jazz', 'Punk', 'Rock', 'Rap',
@@ -136,7 +151,7 @@ st.title('''
 #          'Turntablism/platinisme', 'Tecktonik', 'Electro', 'Folk',
 #          'Jazz', 'Pop rock', 'Reggae', 'Country', 'Blues',
 #          'Rock garage', 'World', 'Ska', 'Raï', 'Gypsy pop', 'Tango', 'Opérette', 'Variété']
-input = ['Default', 'Rap', 'Electro', 'Rock', ]
+input = ['Default', 'Any song', 'Rap', 'Electro', 'Rock', ]
 
 dic = {
     'in': input,
@@ -160,22 +175,22 @@ option = st.selectbox(
 st.cache()
 if option == 'Default':
     st.write('Choose a style !')
-    
 elif option == 'Any song':
     # User can write a song to search
     st.write('Write your song')
     entry = st.text_input("Enter a song to search for its similarities", "Type here ...")
-    if(st.button('Submit')):
-    songToSearch = str(entry)
-    Similarities(songToSearch)
-    
+    if st.button('Submit'):
+        songToSearch = str(entry)
+        toprint = similarities(songToSearch)
+        st.write(toprint[0])
+        st.write(toprint[1])
+        st.write(toprint[2])
+        st.write(toprint[3])
 else:
     # Selection among pre-defined songs
-   out = dic['out'][option]
-   'Check for this: ', out
-   st.write('Want to learn more about these?')
-
-
+    out = dic['out'][option]
+    'Check for this: ', out
+    st.write('Want to learn more about these?')
 
 # '''Show all results as buttons'''
 # liste_artistes = out.split(',')
@@ -187,17 +202,29 @@ if option == 'Rap':
     Booba = st.button('Booba')
     Nekfeu = st.button('Nekfeu')
 if option == 'Rap' and Booba:
-    url = "https://www.youtube.com/watch?v=Stet_4bnclk"     
-    st.write('Try this one: [DKR](%s)' % url)
+    url1 = "https://www.youtube.com/watch?v=Stet_4bnclk"
+    url2 =  "https://www.youtube.com/watch?v=R3VhomP5b10"
+    url3 = "https://www.youtube.com/watch?v=hwtLqJyhS3Q"
+    url4 =  "https://www.youtube.com/watch?v=OXewKWKvva8"
+    url5 =  "https://www.youtube.com/watch?v=s4JbpWWV8sM&pp=sAQA"
+    st.write('These ones are among the biggest hits: [DKR](%s)' % url1, ', [Habibi](%s)' % url2, ', [92i Veyron](%s)' % url3, ', [Pinocchio(ft. Damso & Gato)](%s)' % url4, ', [Comme une étoile](%s)' % url5)
 if option == 'Rap' and Nekfeu:
-    url = "https://www.youtube.com/watch?v=YltjliK0ZeA"
-    st.write('This one is cool: [On verra bien](%s)' % url)
+    url1 = "https://www.youtube.com/watch?v=YltjliK0ZeA&pp=sAQA"
+    url2 =  "https://www.youtube.com/watch?v=Z68u6dJqoI0&pp=sAQA"
+    url3 = "https://www.youtube.com/watch?v=cBKGKkQnI94"
+    url4 =  "https://www.youtube.com/watch?v=V_S-bDdY1lA"
+    url5 =  "https://www.youtube.com/watch?v=eZC2Ohdk-wI"
+    st.write('These ones are among the biggest hits: [On verra bien](%s)' % url1, ', [Ma Dope(ft. SPri Noir)](%s)' % url2, ', [Elle pleut(ft. Nemir)](%s)' % url3, ', ["Le regard des gens(ft. Nemir, 2zer, Mekra, Doums)"](%s)' % url4, ', [Cheum](%s)' % url5)
 
 if option == 'Electro':
     DavidGuetta = st.button('David Guetta')
 if option == 'Electro' and DavidGuetta:
-    url = "https://www.youtube.com/watch?v=JRfuAukYTKg"
-    st.write('The biggest hit is: [Titanium(ft. SIA)](%s)' % url)
+    url1 = "https://www.youtube.com/watch?v=JRfuAukYTKg"
+    url2 =  "https://www.youtube.com/watch?v=NUVCQXMUVnI"
+    # url3 = "https://www.youtube.com/watch?v=cBKGKkQnI94"
+    # url4 =  "https://www.youtube.com/watch?v=V_S-bDdY1lA"
+    # url5 =  "https://www.youtube.com/watch?v=eZC2Ohdk-wI"
+    st.write('These ones are among the biggest hits: [Titanium(ft. SIA)](%s)' % url1, ', [Memories(ft. Kid Cudi)](%s)' % url2)
 
 if option == 'Rock':
     Kyo = st.button('Kyo')
